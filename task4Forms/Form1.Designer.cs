5f6a0a4d3a5feccf8b6b168aada2424ea208ada7
andrey <andrey89066805728@ya.ru>
2020-09-28 23:58:12 +0300

    task4Forms

---
+++ task4Forms/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace task4Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hous = new System.Windows.Forms.Label();
+            this.hous2 = new System.Windows.Forms.Label();
+            this.numericHous1 = new System.Windows.Forms.NumericUpDown();
+            this.numericHous2 = new System.Windows.Forms.NumericUpDown();
+            this.numericHous3 = new System.Windows.Forms.NumericUpDown();
+            this.numeric2Hous1 = new System.Windows.Forms.NumericUpDown();
+            this.numeric2Hous2 = new System.Windows.Forms.NumericUpDown();
+            this.numeric2Hous3 = new System.Windows.Forms.NumericUpDown();
+            this.res1 = new System.Windows.Forms.TextBox();
+            this.res2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.yearNow = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.area = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize) (this.numericHous1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.numericHous2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.numericHous3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.numeric2Hous1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.numeric2Hous2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.numeric2Hous3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.yearNow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hous
+            // 
+            this.hous.Location = new System.Drawing.Point(45, 36);
+            this.hous.Name = "hous";
+            this.hous.Size = new System.Drawing.Size(268, 64);
+            this.hous.TabIndex = 0;
+            this.hous.Text = "hous";
+            // 
+            // hous2
+            // 
+            this.hous2.Location = new System.Drawing.Point(478, 28);
+            this.hous2.Name = "hous2";
+            this.hous2.Size = new System.Drawing.Size(356, 72);
+            this.hous2.TabIndex = 1;
+            this.hous2.Text = "hous2";
+            // 
+            // numericHous1
+            // 
+            this.numericHous1.Location = new System.Drawing.Point(36, 118);
+            this.numericHous1.Name = "numericHous1";
+            this.numericHous1.Size = new System.Drawing.Size(163, 39);
+            this.numericHous1.TabIndex = 2;
+            // 
+            // numericHous2
+            // 
+            this.numericHous2.Location = new System.Drawing.Point(36, 190);
+            this.numericHous2.Name = "numericHous2";
+            this.numericHous2.Size = new System.Drawing.Size(162, 39);
+            this.numericHous2.TabIndex = 3;
+            // 
+            // numericHous3
+            // 
+            this.numericHous3.Location = new System.Drawing.Point(36, 268);
+            this.numericHous3.Name = "numericHous3";
+            this.numericHous3.Size = new System.Drawing.Size(161, 39);
+            this.numericHous3.TabIndex = 4;
+            // 
+            // numeric2Hous1
+            // 
+            this.numeric2Hous1.Location = new System.Drawing.Point(460, 118);
+            this.numeric2Hous1.Name = "numeric2Hous1";
+            this.numeric2Hous1.Size = new System.Drawing.Size(160, 39);
+            this.numeric2Hous1.TabIndex = 5;
+            // 
+            // numeric2Hous2
+            // 
+            this.numeric2Hous2.Location = new System.Drawing.Point(460, 193);
+            this.numeric2Hous2.Name = "numeric2Hous2";
+            this.numeric2Hous2.Size = new System.Drawing.Size(159, 39);
+            this.numeric2Hous2.TabIndex = 6;
+            // 
+            // numeric2Hous3
+            // 
+            this.numeric2Hous3.Location = new System.Drawing.Point(460, 266);
+            this.numeric2Hous3.Name = "numeric2Hous3";
+            this.numeric2Hous3.Size = new System.Drawing.Size(159, 39);
+            this.numeric2Hous3.TabIndex = 7;
+            // 
+            // res1
+            // 
+            this.res1.Location = new System.Drawing.Point(34, 563);
+            this.res1.Name = "res1";
+            this.res1.Size = new System.Drawing.Size(161, 39);
+            this.res1.TabIndex = 8;
+            // 
+            // res2
+            // 
+            this.res2.Location = new System.Drawing.Point(461, 563);
+            this.res2.Name = "res2";
+            this.res2.Size = new System.Drawing.Size(159, 39);
+            this.res2.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 636);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 59);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(459, 636);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 56);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(239, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 44);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "bild year";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(228, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 38);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "count of flats";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(229, 230);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 36);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "number of hous";
+            // 
+            // yearNow
+            // 
+            this.yearNow.Location = new System.Drawing.Point(218, 500);
+            this.yearNow.Name = "yearNow";
+            this.yearNow.Size = new System.Drawing.Size(215, 39);
+            this.yearNow.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(218, 450);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(188, 47);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "year now";
+            // 
+            // area
+            // 
+            this.area.Location = new System.Drawing.Point(460, 412);
+            this.area.Name = "area";
+            this.area.Size = new System.Drawing.Size(158, 39);
+            this.area.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(464, 354);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(155, 47);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "area";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 757);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.area);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.yearNow);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.res2);
+            this.Controls.Add(this.res1);
+            this.Controls.Add(this.numeric2Hous3);
+            this.Controls.Add(this.numeric2Hous2);
+            this.Controls.Add(this.numeric2Hous1);
+            this.Controls.Add(this.numericHous3);
+            this.Controls.Add(this.numericHous2);
+            this.Controls.Add(this.numericHous1);
+            this.Controls.Add(this.hous2);
+            this.Controls.Add(this.hous);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize) (this.numericHous1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.numericHous2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.numericHous3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.numeric2Hous1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.numeric2Hous2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.numeric2Hous3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.yearNow)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label hous2;
+        private System.Windows.Forms.Label hous;
+        private System.Windows.Forms.TextBox res2;
+        private System.Windows.Forms.TextBox res1;
+        private System.Windows.Forms.NumericUpDown numeric2Hous3;
+        private System.Windows.Forms.NumericUpDown numeric2Hous2;
+        private System.Windows.Forms.NumericUpDown numeric2Hous1;
+        private System.Windows.Forms.NumericUpDown numericHous3;
+        private System.Windows.Forms.NumericUpDown numericHous2;
+        private System.Windows.Forms.NumericUpDown numericHous1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown yearNow;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox area;
+    }
+}
---
+++ task4Forms/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task4Forms
+{
+    public partial class Form1 : Form
+    {
+        private task4.Hous Hous;
+        private task4.Hous2 Hous2;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            Hous = new task4.Hous();
+            Hous2 = new task4.Hous2();
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hous.BildYear = (int) numericHous1.Value;
+            Hous.CountOfFlats = (int) numericHous2.Value;
+            Hous.NumberOfHous = (int) numericHous3.Value;
+            res1.Text = Hous.Q((int) yearNow.Value).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Hous2.BildYear = (int) numeric2Hous1.Value;
+            Hous2.CountOfFlats = (int) numeric2Hous2.Value;
+            Hous2.NumberOfHous = (int) numeric2Hous3.Value;
+            Hous2.Area = area.Text;
+            res2.Text = Hous2.Q((int) yearNow.Value).ToString();
+        }
+    }
+}
