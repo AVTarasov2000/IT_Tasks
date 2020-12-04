222203c5d6b5c0da962e18f1631dd225a6a5299f
AVTarasov <andrey89066805728@ya.ru>
2020-12-04 15:35:15 +0300

    7

---
+++ task7Forms/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace task7Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.FunctionName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ButtonColumn = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.chooseFileButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(207, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(195, 381);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {this.FunctionName, this.ButtonColumn});
+            this.dataGridView1.Location = new System.Drawing.Point(628, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(265, 381);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // FunctionName
+            // 
+            this.FunctionName.HeaderText = "Function";
+            this.FunctionName.Name = "FunctionName";
+            this.FunctionName.ReadOnly = true;
+            // 
+            // ButtonColumn
+            // 
+            this.ButtonColumn.HeaderText = "Execute";
+            this.ButtonColumn.Name = "ButtonColumn";
+            this.ButtonColumn.ReadOnly = true;
+            // 
+            // chooseFileButton
+            // 
+            this.chooseFileButton.Location = new System.Drawing.Point(10, 12);
+            this.chooseFileButton.Name = "chooseFileButton";
+            this.chooseFileButton.Size = new System.Drawing.Size(191, 194);
+            this.chooseFileButton.TabIndex = 2;
+            this.chooseFileButton.Text = "button1";
+            this.chooseFileButton.UseVisualStyleBackColor = true;
+            this.chooseFileButton.Click += new System.EventHandler(this.chooseFileButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(408, 13);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(214, 381);
+            this.listBox2.TabIndex = 3;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1010, 405);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.chooseFileButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listBox1);
+            this.Location = new System.Drawing.Point(15, 15);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.DataGridViewButtonColumn ButtonColumn;
+        private System.Windows.Forms.Button chooseFileButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FunctionName;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
+        #endregion
+    }
+}
---
+++ task7Forms/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using task7Library;
+
+namespace task7Forms
+{
+    public partial class Form1 : Form
+    {
+        public ClassLooker ClassLooker { get; set; }
+        public Type SelectedType { get; set; }
+        
+        public object SelectedTypeObj { get; set; }
+        
+        public Form1()
+        {
+            ClassLooker = new ClassLooker(@"C:\Users\Andrey\RiderProjects\IT_Tasks\task6Forms\bin\Debug\task6Library.dll");
+            InitializeComponent();
+            foreach (var type in ClassLooker.AllAbstractClassesAndInterfaces())
+            {
+                listBox1.Items.Add(type.Name);
+            }
+            
+        }
+
+        private void chooseFileButton_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            
+            string filename = openFileDialog1.FileName;
+            ClassLooker = new ClassLooker(filename);
+            foreach (var type in ClassLooker.AllAbstractClassesAndInterfaces())
+            {
+                listBox1.Items.Add(type.Name);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                listBox2.Items.Clear();
+                foreach (var allChildrenAndImpl in ClassLooker.AllChildrenAndImpls(ClassLooker.GetType(listBox1.SelectedItem.ToString())))
+                {
+                    listBox2.Items.Add(allChildrenAndImpl.Name);
+                }
+            }
+        }
+        
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                dataGridView1.Rows.Clear();
+                SelectedType = ClassLooker.GetType(listBox2.SelectedItem.ToString());
+                SelectedTypeObj = Activator.CreateInstance(SelectedType);
+                foreach (var methodInfo in SelectedType.GetMethods())
+                {
+                    DataGridViewRow row = new DataGridViewRow();
+                    DataGridViewCell cell = new DataGridViewTextBoxCell();
+                    cell.Value = methodInfo.Name;
+                    row.Cells.Add(cell);
+                    dataGridView1.Rows.Add(row);
+                }
+            }
+        }
+        
+        
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView1.Columns["ButtonColumn"].Index)
+            {
+                List<Object> objects = new List<object>();
+                MethodInfo methodInfo = SelectedType.GetMethods()[e.RowIndex];
+                foreach (var parameterInfo in methodInfo.GetParameters())
+                {
+                    objects.Add(null);
+                }
+
+                object res = methodInfo.Invoke(SelectedTypeObj, objects.ToArray());
+                MessageBox.Show((string)res);
+            }
+        }
+        
+    }
+}
