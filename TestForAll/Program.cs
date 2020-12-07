07e63ec7180dfe4759b38c3734832f05d6ec8c0e
andrejtarasov <tarasov_av@cs.vsu.ru>
2020-12-07 21:35:20 +0300

    add test

---
+++ TestForAll/Program.cs
@@ -0,0 +1,13 @@
+﻿using task8Library;
+
+namespace TestForAll
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Emulation emulation = new Emulation();
+            emulation.Start();
+        }
+    }
+}
